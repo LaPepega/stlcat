3644afc016d4cdc4788afed77bbd5ddd07d874b2
LaPepega <tmv03092008@gmail.com>
2025-07-13 21:03:51 +0500

    Args with argp

---
+++ include/stlcat_args.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <stdlib.h>
+#include <argp.h>
+
+const char *argp_program_version = "stlcat 0.0.1";
+const char *argp_program_bug_address = "https://github.com/LaPepega/stlcat/issues";
+static char doc[] = "Render an .stl file right into your terminal";
+static char args_doc[] = "stlcat [OPTION] <path/to/file.stl>";
+
+static struct argp_option options[] = {
+    {"stats", 's', 0, 0, "Show render stats", 0},
+    {"width", 'w', "WIDTH", 0, "Render width in pixels\t\tdefault: 100", 0},
+    {"height", 'h', "HEIGHT", 0, "Render height in pixels\t\tdefault: 100", 0},
+    {"camX", 'x', "CAMX", 0, "Camera's position along X axis\tdefault: 2", 0},
+    {"camY", 'y', "CAMY", 0, "Camera's position along Y axis\tdefault: 0", 0},
+    {"camZ", 'z', "CAMZ", 0, "Camera's position along Z axis\tdefault: 0", 0},
+    {"fov", 'f', "FOV", 0, "Camera's FOV (smaller number = wider angle)\tdefault: 20", 0},
+    {0}};
+
+struct arguments
+{
+    bool stats_enabled;
+    unsigned long width, height;
+    float cam_x, cam_y, cam_z, fov;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+    struct arguments *args = (struct arguments *)state->input;
+    switch (key)
+    {
+    case 's':
+        args->stats_enabled = true;
+        break;
+    case 'w':
+        args->width = strtoul(arg, NULL, 10);
+        break;
+    case 'h':
+        args->height = strtoul(arg, NULL, 10);
+        break;
+    case 'x':
+        args->cam_x = strtof(arg, NULL);
+        break;
+    case 'y':
+        args->cam_y = strtof(arg, NULL);
+        break;
+    case 'z':
+        args->cam_z = strtof(arg, NULL);
+        break;
+    case 'f':
+        args->fov = strtof(arg, NULL);
+        break;
+
+    case ARGP_KEY_INIT:
+        break;
+
+    case ARGP_KEY_NO_ARGS:
+        argp_error(state, "No file specified");
+        break;
+
+    case ARGP_KEY_FINI:
+        break;
+
+    case ARGP_KEY_END:
+        break;
+
+    case ARGP_KEY_SUCCESS:
+        break;
+
+    case ARGP_KEY_ARG:
+        break;
+
+    default:
+        argp_usage(state);
+        return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = {
+    .options = options,
+    .parser = parse_opt,
+    .args_doc = args_doc,
+    .doc = doc,
+    .children = 0,
+    .help_filter = 0,
+    .argp_domain = 0};
--- src/stlcat.c
+++ src/stlcat.c
@@ -1,52 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <time.h>
+#include <stdbool.h>
 
+#include "stlcat_args.h"
 #include "solid.h"
 #include "render.h"
 #include "terminal.h"
 
-// FIXME: y changes when all p c and t in the same plain
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    if (argc < 3)
-    {
-        printf("Not enough args");
-        return 1;
-    }
+    struct arguments args = {
+        .cam_x = 2.0f,
+        .cam_y = 0.0f,
+        .cam_z = 0.0f,
+        .fov = 20,
+        .height = 100,
+        .width = 100,
+        .stats_enabled = false,
+    };
+
+    argp_parse(&argp, argc, argv, 0, 0, &args);
 
     double read_time, render_time;
-    clock_t start = clock();
+    clock_t start, end;
 
-    // solid sol = solid_new_from_file_bin(argv[1]);
-
-    /*vec3 target = VEC3(
-        strtof(argv[1], NULL),
-        strtof(argv[2], NULL),
-        strtof(argv[3], NULL));*/
-
-    vec3 camera = VEC3(2, 0, 0);
+    vec3 camera = VEC3(args.cam_x, args.cam_y, args.cam_z);
     vec3 target = VEC3(0, 0, 0);
-    canvas c = canvas_new_of_size(VEC2U(300, 170));
+    canvas c = canvas_new_of_size(VEC2U(args.width, args.height));
 
-    float fov = strtof(argv[1], NULL);
-    solid s = solid_new_from_file_bin(argv[2]);
+    start = clock();
+    solid s = solid_new_from_file_bin(argv[argc - 1]);
+    end = clock();
 
-    clock_t end = clock();
     read_time = ((double)(end - start) / CLOCKS_PER_SEC) * 1000;
 
     start = clock();
-
-    canvas_render_solid(&c, &s, &camera, &target, fov);
-
+    canvas_render_solid(&c, &s, &camera, &target, args.fov);
     end = clock();
-
-    canvas_print(&c);
 
     render_time = ((double)(end - start) / CLOCKS_PER_SEC) * 1000;
 
-    printf("tris:%lu, points:%lu, read_ms: %lf, render_ms: %lf\n", s.n_tris, s.n_tris * 3, read_time, render_time);
+    canvas_print(&c);
+
+    if (args.stats_enabled)
+    {
+        printf("tris:%lu, points:%lu, read_ms: %lf, render_ms: %lf\n", s.n_tris, s.n_tris * 3, read_time, render_time);
+    }
 
     canvas_free(&c);
     solid_free(&s);
